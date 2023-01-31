4bc54bd0c81ddfc4ed62c8748818f3c3bcc9879b
Daniel Król <danielkrol90@gmail.com>
2023-01-31 20:36:26 +0100

    preparing folders and filers to work on them

---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import About from './about/About.jsx';
+import Contact from './contact/Contact.jsx';
+import Gallery from './gallery/Gallery.jsx';
+import Home from './home/Home.jsx';
+import NotFound from './notFound/NotFound.jsx';
+import Plans from './plans/Plans.jsx';
+import Trainers from './trainers/Trainers.jsx';
+
+export {
+    About,
+    Contact,
+    Gallery,
+    Home,
+    NotFound,
+    Plans,
+    Trainers
+};
