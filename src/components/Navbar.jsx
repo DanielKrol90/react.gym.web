27936629ab66f32f2a5f45dd82ee0e2a32594698
Daniel Król <danielkrol90@gmail.com>
2023-02-01 21:12:18 +0100

    working on hide.show navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Link, NavLink } from 'react-router-dom';
+import { useState } from 'react';
+
 import './Navbar.css'
 import { logo1 } from '../images/index'
 import { links } from '../data';
@@ -7,24 +9,25 @@ import { FaBars } from 'react-icons/fa'
 
 
 const Navbar = () => {
+    const { isNavShowing, setIsNavShowing } = useState(false);
     return (
         <nav>
             <div className="container nav__container">
                 <Link to="/" className='logo'>
                     <img src={logo1} alt="Nav Logo" />
                 </Link>
-                <ul className="nav__links">
+                <ul className={`nav__links ${isNavShowing ? 'show_nav' : 'hide__nav'}`}>
                     {
                         links.map(({ name, path }, index) => {
                             return (
                                 <li key={name}>
-                                    <NavLink to={path}>{name}</NavLink>
+                                    <NavLink to={path} className={({isActive}) => isActive ? 'active-nav' : ''}>{name}</NavLink>
                                 </li>
                             )
                         })
                     }
                 </ul>
-                <button className='nav__toggle-btn'>
+                <button className='nav__toggle-btn' onClick={() => setIsNavShowing(!isNavShowing)}>
                     <FaBars />
                 </button>
             </div>
